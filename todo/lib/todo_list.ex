d6bf517ed3656a7cb16d40e3ad12b07884439c78
Michael Campbell <michael.campbell@gmail.com>
2024-08-20 21:04:25 -0400

    abstracted a todo list entry as a map instead of discrete fields

--- todo/lib/todo_list.ex
+++ todo/lib/todo_list.ex
@@ -16,20 +16,35 @@ defmodule TodoList do
   def new, do: MultiDict.new()
 
   @doc """
-  Add a new item to the TodoList list.
+  Add a new item to the TodoList list.  Accepts a map with a `date` and a `title` field.
 
   ## Examples
 
       iex> t = TodoList.new()
-      iex> t = TodoList.add_entry(t, ~D[2016-01-01], "Buy milk")
+      iex> t = TodoList.add_entry(t, %{date: ~D[2016-01-01], title: "Buy milk"})
       iex> t
       %{~D[2016-01-01] => ["Buy milk"]}
 
       iex> t = TodoList.new()
-      iex> t = TodoList.add_entry(t, ~D[2016-01-01], "Buy milk")
-      iex> t = TodoList.add_entry(t, ~D[2016-01-01], "Buy more milk")
+      iex> t = TodoList.add_entry(t, %{date: ~D[2016-01-01], title: "Buy milk"})
+      iex> t = TodoList.add_entry(t, %{date: ~D[2016-01-01], title: "Buy more milk"})
       iex> t
       %{~D[2016-01-01] => ["Buy more milk", "Buy milk"]}
   """
-  def add_entry(todo_list, date, title), do: MultiDict.add(todo_list, date, title)
+  def add_entry(todo_list, entry), do: MultiDict.add(todo_list, entry.date, entry.title)
+
+  @doc """
+  Get the entries for a given date.
+
+  ## Examples
+
+      iex> t = TodoList.new()
+      iex> t = TodoList.add_entry(t, %{date: ~D[2016-01-01], title: "Buy milk"})
+      iex> t = TodoList.add_entry(t, %{date: ~D[2016-01-01], title: "Buy more milk"})
+      iex> TodoList.entries(t, ~D[2016-01-01])
+      ["Buy more milk", "Buy milk"]
+      iex> TodoList.entries(t, ~D[1999-01-01])
+      []
+  """
+  def entries(todo_list, date), do: MultiDict.get(todo_list, date)
 end
